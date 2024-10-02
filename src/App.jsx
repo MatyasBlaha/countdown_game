d516ee33c552ea32ee3e249bfb6e051765f7cb05
MatyasBlaha <matyasblaha1@gmail.com>
2024-10-02 11:11:37 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+          <TimerChallenge title='Easy' targetTime={1}/>
+          <TimerChallenge title='medium' targetTime={5}/>
+          <TimerChallenge title='hard' targetTime={10}/>
+          <TimerChallenge title='pro' targetTime={15}/>
+      </div>
     </>
   );
 }
